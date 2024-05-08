aa56afda2fbc193936cc897c5c641420dd5c04c7
wt <yuan364299311@gmail.com>
2024-05-08 20:58:57 +0800

    feat: update code;

--- .vuepress/sidebars/frontEndLearn.ts
+++ .vuepress/sidebars/frontEndLearn.ts
@@ -4,8 +4,8 @@ export default [
         title: "自动化部署",
         collapsable: true,
         children: [
-            '自动化部署/GitHub的WebHooks的使用.md',
-            '自动化部署/WebHooks结合服务器管理的使用.md'
+            './自动化部署/GitHub的WebHooks的使用.md',
+            './自动化部署/WebHooks结合服务器管理的使用.md'
         ]
     }
 ]
