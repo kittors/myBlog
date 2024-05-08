f5b92852c72f4b2e2793c3e53edc520ca2d7be52
wt <yuan364299311@gmail.com>
2024-05-08 22:40:07 +0800

    feat: update code;

--- .vuepress/sidebars/frontEndLearn.ts
+++ .vuepress/sidebars/frontEndLearn.ts
@@ -4,8 +4,8 @@ export default [
         title: "脚本化部署web项目",
         collapsable: true,
         children: [
-            'AutomatedDeployment/UseOfGitHubWebHooks.md',
-            'AutomatedDeployment/TheUseOfWebHooksCombinedWithServerManagement.md',
+            'AutomatedDeployment/GitHub的WebHooks的使用.md',
+            'AutomatedDeployment/WebHooks结合服务器管理的使用.md',
         ]
     }
 ]
--- ProgrammingLearning/README.md
+++ ProgrammingLearning/README.md
@@ -4,4 +4,4 @@
 >
 > 本站地址：[https://blog.07230805.xyz](https://blog.07230805.xyz)
 
-[自动化部署](自动化部署/自动化部署.md)
+[自动化部署](AutomatedDeployment/自动化部署.md)
